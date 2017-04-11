833d36bdf85924a5f3af182901fbd86d93bbdec3
Mehul Vibhakar <Mehul Vibhakar>
2017-04-11 16:10:24 -0700

    modified gitignore

--- startAmazon.sh
+++
@@ -1,2 +0,0 @@
-ssh -i "tensorflowkeypair.pem" ubuntu@ec2-54-200-161-191.us-west-2.compute.amazonaws.com
-# scp -i "tensorflowkeypair.pem" tensorflow/code.zip ubuntu@ec2-54-200-161-191.us-west-2.compute.amazonaws.com:
