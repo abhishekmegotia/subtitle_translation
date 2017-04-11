efcb0c0afdb403979157b1f913ec8a7e30e1dc02
Mehul Vibhakar <Mehul Vibhakar>
2017-04-11 16:06:19 -0700

    added new translation code removed unwanted files

---
+++ startAmazon.sh
@@ -0,0 +1,2 @@
+ssh -i "tensorflowkeypair.pem" ubuntu@ec2-54-200-161-191.us-west-2.compute.amazonaws.com
+# scp -i "tensorflowkeypair.pem" tensorflow/code.zip ubuntu@ec2-54-200-161-191.us-west-2.compute.amazonaws.com:
